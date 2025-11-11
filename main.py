91c5dba88b36a6e010e1e2d24b4961620c742bdc
hexne <printfne@gmail.com>
2025-11-11 15:49:23 +0800

    ？

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def train(c):
         data=f"dataset/{c}/dataset.yaml",
         imgsz=512,
         epochs=300,
-        batch=64,
+        batch=32,
         workers=2,
         device=0,
         name=f"{c}",
